af13257efabed9569dde1b2656b43f7d7266f8f2
Hongyu Lu <sannrei@hotmail.com>
2023-09-17 19:19:19 +0200

    Higtlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,11 +7,12 @@ import useGenres, {Genre} from '../hooks/useGenres'
 
 interface Props{
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null
 }
 
 
 
-const GenreList = ({onSelectGenre}:Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
     const {data , isLoading , error} = useGenres()
     
     if (error) return null
@@ -23,7 +24,7 @@ const GenreList = ({onSelectGenre}:Props) => {
             <ListItem key={genre.id} paddingY='5px'> 
             <HStack> 
               <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background) }/>
-              <Button onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link' >{genre.name}</Button>
+              <Button fontWeight={genre.id === selectedGenre?.id? 'bold': 'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link' >{genre.name}</Button>
                
             </HStack></ListItem>
             )}
