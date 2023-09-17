669f7d2d23b312b091b6caf0975fae8e565ddb26
Hongyu Lu <sannrei@hotmail.com>
2023-09-17 23:23:15 +0200

    Built search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import {Grid ,GridItem,Show,HStack,Flex,Box} from '@chakra-ui/react'
+import {Grid ,GridItem,Show,Flex,Box} from '@chakra-ui/react'
 import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
